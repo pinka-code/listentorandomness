b88a7ec6f672dcb9a0fc38d509bbb9ffbe146ba1
pinka-code <sylvain.tiset@gmail.com>
2026-02-15 19:52:51 +0100

    feat(mesure): add customization for some instruments

--- mesure.py
+++ mesure.py
@@ -13,16 +13,54 @@ class Mesure:
     def jouer(self, instr, time_start, nuance_phrase):
         time = time_start
         motif_idx = 0
-        nb_notes = len(self.motif)
         for duration in self.motif_rythmique:
+
+            if self.role == "basse":
+                # ne joue que sur temps forts
+                if int(time - time_start) % 1 != 0:
+                    time += duration
+                    continue
+            if self.role == "pad":
+                # ne joue qu'une note par mesure
+                if motif_idx > 0:
+                    time += duration
+                    continue
+
             # silence aléatoire
             if rythme.generer_silence(self.rng, probabilite=0.1):
                 time += duration
                 continue
 
-            degre = self.motif[motif_idx % nb_notes]
+            if self.role == "basse":
+                # privilégie tonique et quinte
+                degre = self.rng.choice([0, 4])
+
+            elif self.role == "harmonie":
+                # privilégie notes stables
+                degre = self.rng.choice([0, 2, 4])
+
+            else:
+                # comportement normal
+                degre = self.motif[motif_idx % len(self.motif)]
+
             note_base = self.config.notes_gamme[degre]
-            octave = octaves.choisir_octave(self.rng)
+            
+            if self.role == "basse":
+                octave = self.rng.choice([1, 2])
+
+            elif self.role == "melodie":
+                octave = self.rng.choice([4, 5])
+
+            elif self.role == "contrechant":
+                octave = self.rng.choice([3, 4])
+
+            elif self.role == "pad":
+                octave = self.rng.choice([3, 4])
+
+            else:
+                octave = self.rng.choice([3, 4])
+
+
             pitch = note_base + 12 * octave
             pitch = ajuster_pitch_au_registre(pitch, self.nom_instrument)
 
@@ -30,10 +68,23 @@ class Mesure:
             duree_mesure = sum(self.motif_rythmique)
             if time + duration > time_start + duree_mesure:
                 duration = time_start + duree_mesure - time
+            if duration <= 0:
+                break
 
             velocity = nuance_phrase
             if (time - time_start) % 1 == 0:  # accent sur temps fort
                 velocity = min(127, velocity + 10)
+
+            if self.role == "melodie":
+                velocity += 10
+
+            elif self.role == "basse":
+                velocity += 5
+
+            elif self.role == "pad":
+                velocity -= 10
+
+            velocity = max(20, min(127, velocity))
 
             note = pretty_midi.Note(
                 pitch=pitch,
