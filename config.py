78b63c4ea971a2479843f8064afaf047b4debf44
pinka-code <sylvain.tiset@gmail.com>
2026-02-15 10:11:58 +0100

    feat(rythm-signatures): add rythmic signature

--- config.py
+++ config.py
@@ -6,5 +6,9 @@ class MusicConfig:
     gamme_notes: list
     tempo_nom: str
     tempo_bpm: int
+    signature_nom: str
+    signature_num: int
+    signature_den: int
+    signature_type: str
     num_pistes: int
     duree_totale: float
--- main.py
+++ main.py
@@ -12,6 +12,7 @@ config = structure.generer_structure(random_generator)
 print("===== CONFIGURATION DU MORCEAU =====")
 print(f"Tonalité : {config.gamme_nom}")
 print(f"Tempo : {config.tempo_nom} → {config.tempo_bpm} BPM")
+print(f"Signature rythmique : {config.signature_nom}")
 print(f"Nombre de pistes : {config.num_pistes}")
 print(f"Durée totale cible : {config.duree_totale} sec")
 print("=====================================")
@@ -19,6 +20,13 @@ print("=====================================")
 # Création du MIDI
 midi = pretty_midi.PrettyMIDI()
 midi._PrettyMIDI__initial_tempo = config.tempo_bpm
+midi.time_signature_changes.append(
+    pretty_midi.TimeSignature(
+        config.signature_num,
+        config.signature_den,
+        0  # début du morceau
+    )
+)
 
 for piste_id in range(config.num_pistes):
 
